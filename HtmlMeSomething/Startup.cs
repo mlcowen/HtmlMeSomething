f0589ab547106e89d450d6e4e3240c84c3757039
Marie Cowen <marie..cowen.87@gmail.com>
2019-04-20 23:12:47 -0500

    first commit

--- HtmlMeSomething/Startup.cs
+++ HtmlMeSomething/Startup.cs
@@ -20,15 +20,9 @@ namespace HtmlMeSomething
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+                app.UseDefaultFiles();
+                app.UseStaticFiles();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
         }
     }
 }
